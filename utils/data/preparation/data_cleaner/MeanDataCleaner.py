767970d4ae54634a52ef4870187bc8964994e367
Vladimir Agafonov <agafonov.vladymyrovich@gmail.com>
2019-11-01 01:11:10 +0200

    Fix MeanDataCleaner

--- utils/data/preparation/data_cleaner/MeanDataCleaner.py
+++ utils/data/preparation/data_cleaner/MeanDataCleaner.py
@@ -22,13 +22,15 @@ class MeanDataCleaner(BaseDataCleaner):
                 else:
                     if j in column_accumulator:
                         column_accumulator[j].append(element)
+                    else:
+                        column_accumulator[j] = [element]
 
         # calculate mean for each column
         for key in column_accumulator.keys():
-            column_accumulator = np.mean(np.array(column_accumulator[key], dtype=np.float32))
+            column_accumulator[key] = np.mean(np.array(column_accumulator[key], dtype=np.float32))
 
         # fill holes
         for coords in holes_coordinates:
-            data[coords[0]][coords[1]] = str(column_accumulator[column_accumulator[coords[1]]])
+            data[coords[0]][coords[1]] = str(column_accumulator[coords[1]])
 
         return data
